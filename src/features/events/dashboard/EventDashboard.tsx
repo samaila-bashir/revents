e06b7baa05b2442d6ab03d877585e0e6e55557cb
samaila-bashir <developer.chatto@gmail.com>
2024-06-26 10:47:59 +0100

    implemented CRUD functionality on events

--- src/features/events/dashboard/EventDashboard.tsx
+++ src/features/events/dashboard/EventDashboard.tsx
@@ -8,22 +8,63 @@ import { useEffect, useState } from "react";
 type Props = {
   formOpen: boolean;
   setFormOpen: (value: boolean) => void;
+  selectedEvent: AppEvent | null;
+  selectEvent: (event: AppEvent | null) => void;
 };
 
-export default function EventDashboard({ formOpen, setFormOpen }: Props) {
+export default function EventDashboard({
+  formOpen,
+  setFormOpen,
+  selectEvent,
+  selectedEvent,
+}: Props) {
   const [events, setEvents] = useState<AppEvent[]>([]);
 
   useEffect(() => {
     setEvents(sampleData);
   }, []);
 
+  const addEvent = (event: AppEvent) => {
+    setEvents((prevState) => {
+      return [...prevState, event];
+    });
+
+    setFormOpen(false);
+  };
+
+  const updateEvent = (updatedEvent: AppEvent) => {
+    setEvents((prevState) => {
+      return prevState.map((event) =>
+        event.id === updatedEvent.id ? updatedEvent : event
+      );
+    });
+  };
+
+  const deleteEvent = (eventId: string) => {
+    setEvents((prevState) => {
+      return prevState.filter((event) => event.id !== eventId);
+    });
+  };
+
   return (
     <Grid>
       <Grid.Column width={10}>
-        <EventList events={events} />
+        <EventList
+          events={events}
+          selectEvent={selectEvent}
+          deleteEvent={deleteEvent}
+        />
       </Grid.Column>
       <Grid.Column width={6}>
-        {formOpen && <EventForm setFormOpen={setFormOpen} />}
+        {formOpen && (
+          <EventForm
+            setFormOpen={setFormOpen}
+            updateEvent={updateEvent}
+            addEvent={addEvent}
+            selectedEvent={selectedEvent}
+            key={selectedEvent ? selectedEvent.id : "create"}
+          />
+        )}
       </Grid.Column>
     </Grid>
   );
--- src/features/events/form/EventForm.tsx
+++ src/features/events/form/EventForm.tsx
@@ -1,31 +1,110 @@
+import { ChangeEvent, useState } from "react";
 import { Button, Form, Header, Segment } from "semantic-ui-react";
+import { AppEvent } from "../../../app/types/events";
+import { createId } from "@paralleldrive/cuid2";
 
 type Props = {
   setFormOpen: (value: boolean) => void;
+  addEvent: (event: AppEvent) => void;
+  selectedEvent: AppEvent | null;
+  updateEvent: (event: AppEvent) => void;
 };
 
-export default function EventForm({ setFormOpen }: Props) {
+export default function EventForm({
+  setFormOpen,
+  addEvent,
+  selectedEvent,
+  updateEvent,
+}: Props) {
+  const initialValues = selectedEvent ?? {
+    title: "",
+    category: "",
+    description: "",
+    city: "",
+    venue: "",
+    date: "",
+  };
+
+  const [formValues, setFormValues] = useState(initialValues);
+
+  const handleFormSubmit = () => {
+    // e.preventDefault();
+    selectedEvent
+      ? updateEvent({ ...selectedEvent, ...formValues })
+      : addEvent({
+          ...formValues,
+          id: createId(),
+          hostedBy: "Chatto",
+          attendees: [],
+          hostPhotoURL: "",
+        });
+
+    setFormOpen(false);
+  };
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    const newFormValues = { ...formValues, [name]: value };
+    setFormValues(newFormValues);
+  };
+
   return (
     <Segment clearing>
-      <Header content="Create Event" />
-      <Form>
+      <Header content={selectedEvent ? "Update Event" : "Create Event"} />
+      <Form onSubmit={handleFormSubmit}>
         <Form.Field>
-          <input type="text" placeholder="Event title" />
+          <input
+            type="text"
+            name="title"
+            value={formValues.title}
+            placeholder="Event title"
+            onChange={handleInputChange}
+          />
         </Form.Field>
         <Form.Field>
-          <input type="text" placeholder="Category" />
+          <input
+            type="text"
+            name="category"
+            value={formValues.category}
+            placeholder="category"
+            onChange={handleInputChange}
+          />
         </Form.Field>
         <Form.Field>
-          <input type="text" placeholder="Description" />
+          <input
+            type="text"
+            name="description"
+            value={formValues.description}
+            placeholder="description"
+            onChange={handleInputChange}
+          />
         </Form.Field>
         <Form.Field>
-          <input type="text" placeholder="City" />
+          <input
+            type="text"
+            name="city"
+            value={formValues.city}
+            placeholder="City"
+            onChange={handleInputChange}
+          />
         </Form.Field>
         <Form.Field>
-          <input type="text" placeholder="Venue" />
+          <input
+            type="text"
+            name="venue"
+            value={formValues.venue}
+            placeholder="Venue"
+            onChange={handleInputChange}
+          />
         </Form.Field>
         <Form.Field>
-          <input type="text" placeholder="Date" />
+          <input
+            type="date"
+            name="date"
+            value={formValues.date}
+            placeholder="Date"
+            onChange={handleInputChange}
+          />
         </Form.Field>
 
         <Button type="submit" floated="right" positive content="Submit" />
