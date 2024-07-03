46d8fb9b52f6211cf65befc3de2a927d2ed57946
samaila-bashir <developer.chatto@gmail.com>
2024-07-03 01:35:21 +0100

    created a generic slice

---
+++ src/app/store/slices/genericSlice/index.ts
@@ -0,0 +1,56 @@
+import {
+  createSlice,
+  SliceCaseReducers,
+  ValidateSliceCaseReducers,
+  PayloadAction,
+  ActionCreatorWithoutPayload,
+  ActionCreatorWithPayload,
+  ActionCreatorWithPreparedPayload,
+  ActionCreatorWithOptionalPayload,
+} from "@reduxjs/toolkit";
+
+type GenericState<T> = {
+  data?: T;
+  status: "loading" | "finished" | "error";
+  errors?: any;
+};
+
+export const createGenericSlice = <
+  T,
+  Reducers extends SliceCaseReducers<GenericState<T>>
+>({
+  name = "",
+  initialState,
+  reducers,
+}: {
+  name: string;
+  initialState: GenericState<T>;
+  reducers: ValidateSliceCaseReducers<GenericState<T>, Reducers>;
+}) => {
+  return createSlice({
+    name,
+    initialState,
+    reducers: {
+      loading: (state) => {
+        state.status = "loading";
+      },
+      success: (state: GenericState<T>, action: PayloadAction<T>) => {
+        state.data = action.payload;
+        state.status = "finished";
+      },
+      error: (state, action) => {
+        state.errors = action.payload;
+        state.status = "error";
+      },
+      ...reducers,
+    },
+  });
+};
+
+export type GenericActions<T> = {
+  loading: ActionCreatorWithoutPayload<string>;
+  success:
+    | ActionCreatorWithPayload<T, string>
+    | ActionCreatorWithPreparedPayload<any, T, string, never, never>;
+  error: ActionCreatorWithOptionalPayload<any, string>;
+};
